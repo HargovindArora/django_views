d82b15ef78814bd84e55c00f3e9d8c553c83cff4
HargovindArora <hargovinds50@gmail.com>
2017-08-06 20:15:53 +0530

    Changes in HTML Pages

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView, ListView, DetailView
-from . models import models
+from . import models
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
 class SchoolListView(ListView):
+    context_object_name = 'schools'
     model = models.School
+    # school_list
 
 class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
