a6a0d3af571c8786c78ec417c3da442a301b55f2
HargovindArora <hargovinds50@gmail.com>
2017-08-07 11:04:42 +0530

    updated

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import (View, TemplateView,
+                                    ListView, DetailView,
+                                    CreateView,UpdateView,
+                                    DeleteView)
 from . import models
 
 class IndexView(TemplateView):
@@ -14,3 +18,15 @@ class SchoolDetailView(DetailView):
     context_object_name = 'school_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name','principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('basic_app:list')
